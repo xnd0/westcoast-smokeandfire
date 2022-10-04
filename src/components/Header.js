6e144cf3b726a0a8110886c5cfd5936850caa85f
xndcmd <xndcmd@gmail.com>
2022-10-04 10:18:00 -0700

    inserted logo image

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import firelogo from './smoke-and-fire-icon.png'
 
 
 export default function Header() {
@@ -6,9 +7,13 @@ export default function Header() {
     return (
         <div className='App-header flex-apart'>
             
-            <p>westcoast smoke and fire map</p>
-            <p>Header</p>
+                <div className='App-header'>
+                <img src={firelogo} alt="fire and smoke logo image" height={30} width={30} />
+                <p>westcoast smoke and fire map</p>
+                </div>
+                <p>Header</p>
             
+
         </div>
     );
 };
