b62d34c04edef4f52e4574e00060684e172ffc35
xndcmd <xndcmd@gmail.com>
2022-10-04 13:06:51 -0700

    map successfully displays

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,4 +1,23 @@
-import React from 'react';
+// import React from 'react';
+
+import React, { useState } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+// import './App.css';
+// import { Icon } from "leaflet";
+// import * as parkData from "./data/skateboard-parks.json";
+
+// var map = L.map('map').setView([34.052, -118.244], 13);
+// var marker = L.marker([51.5, -0.09]).addTo(map);
+
+// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//     maxZoom: 18,
+//     // id: 'mapbox/satellite-v9',
+//     id: 'mapbox/streets-v11',
+//     tileSize: 512,
+//     zoomOffset: -1,
+//     accessToken: 'pk.eyJ1IjoieG5kY21kIiwiYSI6ImNsMGxvdnF4aTB5OWMzYmw0bzRxZWEwaDQifQ.0OevUWqcsyWAe5gbtCUPvQ'
+// }).addTo(map);
 
 
 
@@ -12,7 +31,15 @@ export default function MainPage() {
 
             <div className='main-center'>
                 {/* <p>westcoast smoke and fire map</p> */}
-                <div id='map'>(map goes here)</div>
+                <div id='map'>(map goes here)
+
+                    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+                        <TileLayer
+                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        />
+                    </MapContainer>
+                </div>
             </div>
 
             <div className='main-rightside'>
