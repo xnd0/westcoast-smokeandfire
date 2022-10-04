2599b5a0be6d320289bc06d7f52e1f720ceba3e7
xndcmd <xndcmd@gmail.com>
2022-10-04 13:16:17 -0700

    adjusted map coordinates to load west coast USA

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -31,9 +31,8 @@ export default function MainPage() {
 
             <div className='main-center'>
                 {/* <p>westcoast smoke and fire map</p> */}
-                <div id='map'>(map goes here)
-
-                    <MapContainer center={[45.4, -75.7]} zoom={12} scrollWheelZoom={false}>
+                <div id='map'>
+                    <MapContainer center={[39, -125]} zoom={5} scrollWheelZoom={false}>
                         <TileLayer
                             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
