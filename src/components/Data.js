7148d4dcf512b8c0b6aecc51bc239af549a16bc5
xndcmd <xndcmd@gmail.com>
2022-10-05 14:32:01 -0700

    moved geo data to separate file, cleaned up

---
+++ src/components/Data.js
@@ -0,0 +1,60 @@
+// -- location data file -- //
+// -- currently: 
+// full westcoast map
+// seattle
+// portland
+// eugene
+// san francisco
+// los angeles
+
+
+export const data=[
+    {
+        name: "WestCoast, USA",
+        coordinates: {
+            latitude: "41.1",
+            longitude: "-125",
+        },
+        zoomint: "5"
+    },
+    {
+        name: "Seattle, WA",
+        coordinates: {
+            latitude: "47.606",
+            longitude: "-122.332",
+        },
+        zoomint: "8"
+    },
+    {
+        name: "Portland, OR",
+        coordinates: {
+            latitude: "45.515",
+            longitude: "-122.678",
+        },
+        zoomint: "8"
+    },
+    {
+        name: "Eugene, OR",
+        coordinates: {
+            latitude: "44.052",
+            longitude: "-123.087",
+        },
+        zoomint: "12"
+    },
+    {
+        name: "San Francisco, CA",
+        coordinates: {
+            latitude: "37.775",
+            longitude: "-122.419",
+        },
+        zoomint: "10"
+    },
+    {
+        name: "Los Angeles, CA",
+        coordinates: {
+            latitude: "33.9",
+            longitude: "-118.4",
+        },
+        zoomint: "10"
+    }
+]
--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -4,64 +4,12 @@ import React, { useEffect, useState } from "react";
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import Map from "./Map";
-// import './App.css';
-// import { Icon } from "leaflet";
-// import * as parkData from "./data/skateboard-parks.json";
-
-// var map = L.map('map').setView([34.052, -118.244], 13);
-// var marker = L.marker([51.5, -0.09]).addTo(map);
-
-// L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     // id: 'mapbox/satellite-v9',
-//     id: 'mapbox/streets-v11',
-//     tileSize: 512,
-//     zoomOffset: -1,
-//     accessToken: 'pk.eyJ1IjoieG5kY21kIiwiYSI6ImNsMGxvdnF4aTB5OWMzYmw0bzRxZWEwaDQifQ.0OevUWqcsyWAe5gbtCUPvQ'
-// }).addTo(map);
+import { data } from "./Data";
 
 
 
 export default function MainPage() {
 
-    let latitude = 34;
-    let longitude = -188.5;
-
-    const data = [
-        {
-            name: "WestCoast",
-            coordinates: {
-                latitude: "39",
-                longitude: "-125",
-            },
-            zoomint: "5"
-        },
-        {
-            name: "Seattle",
-            coordinates: {
-                latitude: "47.3",
-                longitude: "-122",
-            },
-            zoomint: "8"
-        },
-        {
-            name: "Los Angeles",
-            coordinates: {
-                latitude: "34",
-                longitude: "-118.5",
-            },
-            zoomint: "10"
-        },
-
-    ];
-
-
-    // const handleLA = () => {
-    //     xcoord = 34;
-    //     ycoord = -118.5;
-    //     console.log(xcoord, ycoord, 'are coords')
-    // }
 
     const [index, setIndex] = useState(0);
 
@@ -72,37 +20,14 @@ export default function MainPage() {
             </div>
 
             <div className='main-center'>
-                {/* <p>westcoast smoke and fire map</p> */}
-                {/* <div id='map'>
-
-                    <MapContainer 
-                    center={[latitude, longitude]} 
-                    zoom={[zoomint]} 
-                    scrollWheelZoom={true}>
-                        <TileLayer
-                            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                        />
-                    </MapContainer>
-                </div> */}
-
-<>
-      <Map data={data} index={index} />
-      {data.map(({ name }, idx) => (
-        <button onClick={() => setIndex(idx)}>{`Goto ${name}`}</button>
-      ))}
-    </>
-
+                    <Map data={data} index={index} />
             </div>
 
             <div className='main-rightside'>
                 <p>(right)</p>
-                {/* <button id='westcoast'>West Coast, USA</button>
-                <button id='sea'>Seattle, WA</button>
-                <button id='pdx'>Portland, OR</button>
-                <button id='eug'>Eugene, OR</button>
-                <button id='sf'>San Francisco, CA</button> */}
-                {/* <button id='la' onClick={handleLA}>Los Angeles, CA</button> */}
+                {data.map(({ name }, idx) => (
+                    <button onClick={() => setIndex(idx)}>{`${name}`}</button>
+                ))}
             </div>
         </div>
     );
