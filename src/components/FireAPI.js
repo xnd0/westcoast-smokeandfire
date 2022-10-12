79529e8141258d511d1bf3b39b0b07cbede3096c
xndcmd <xndcmd@gmail.com>
2022-10-12 15:46:54 -0700

    installed express package

--- src/components/FireAPI.js
+++ src/components/FireAPI.js
@@ -1,5 +1,10 @@
 import React from "react";
 
+// Link for just state of CA
+// https://inciweb.nwcg.gov/feeds/rss/incidents/state/5/ 
+
 export default function FireAPI () {
     console.log('connected FireAPI')
+
+
 }
