3c8f6080a43d0f2d39ca943ebb198185ecc59206
xndcmd <xndcmd@gmail.com>
2022-10-13 12:30:42 -0700

    APItest successfully logs test data from web

--- src/components/APItest.js
+++ src/components/APItest.js
@@ -1,6 +1,24 @@
-import React from "react";
+// import React from "react";
+
+import React, { useState, useEffect } from 'react';
 
 export default function APItest () {
   console.log("APItest begin RUN")
+
+
+  try {
+    const response = fetch(
+      `https://www.googleapis.com/books/v1/volumes?q=test`
+
+    );
+
+    console.log(response)
+
+  } catch (err) {
+    console.error(err);
+  }
+
+
+
 }
-   
+
